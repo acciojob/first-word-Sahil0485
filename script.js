21bd456cb812a86899ba9432152e731256d7c259
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-08-22 22:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	if(s === '') return '';
+	let spaceIndex = s.indexOf(' ');
+
+  if (spaceIndex === -1) return s;
+
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
